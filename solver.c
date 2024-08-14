8bfbc2e59d36c9124497b8a439d49e07b385f6f7
Afobaboa <120049289+Afobaboa@users.noreply.github.com>
2024-08-14 20:33:07 +0400

    Add files via upload
    solver.c is created

---
+++ solver.c
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "squareSolver.h"
+
+#define EPSILON 0.000001
+
+
+
+rootCount_t solveEquation(double a, double b, double c, answer_t* answer);
+static rootCount_t solveLinearEquation(double a, double b, answer_t* answer);
+static rootCount_t solveSquareEquation(double a, double b, double c, answer_t* answer);
+static double getDiscriminant(double a, double b, double c);
+static bool isNull(double num);
+
+
+
+// Solve ax^2 + bx + c = 0, a CAN be 0
+rootCount_t solveEquation(double a, double b, double c, answer_t* answer) {
+    if (isNull(a))
+        return solveLinearEquation(b, c, answer);
+    return solveSquareEquation(a, b, c, answer);
+}
+
+
+// Solve ax^2 + bx + c = 0, a CAN'T be 0
+static rootCount_t solveEquation(double a, double b, double c, answer_t* answer) {
+    double discr = getDiscriminant(a, b, c);
+    if (isNull(discr)) {
+        answer[0] = -b / (2*a);
+        return ONE_ROOT;
+    } else if (discr < 0) 
+        return NO_ROOTS;
+    else {
+        answer[0] = (-b - sqrt(discr)) / (2*a);
+        answer[1] = (-b + sqrt(discr) / (2*a));
+        return TWO_ROOTS;
+    }
+}
+
+
+// Solve ax + b = 0
+static rootCount_t solveLinearEquation(double a, double b, answer_t* answer) {
+    if (isNull(a)) 
+        if (isNull(b))
+            return INFINITE_ROOT_COUNT;
+        else
+            return NO_ROOTS;
+    answer[0] = -b/a;
+    return ONE_ROOT;
+}
+
+
+// Get discriminant
+static double getDiscriminant(double a, double b, double c) {
+    return b*b - 4*a*c;
+}
+
+
+// Is num a double 0?
+static bool isNull(double num) {
+    if (abs(num) < EPSILON)
+        return true;
+    return false;
+}
---
+++ squareSolver.h
@@ -0,0 +1,18 @@
+#ifndef SQUARE_SOLVER_H
+#define SQUARE_SOLVER_H
+
+
+#define MAX_ROOT_COUNT 2
+
+enum ROOT_COUNT {
+    INFINITE_ROOT_COUNT,
+    NO_ROOTS,
+    ONE_ROOT,
+    TWO_ROOTS
+};
+
+typedef double answer_t;
+typedef enum ROOT_COUNT rootCount_t;
+
+
+#endif // SQUARE_SOLVER_H
