a84ff9751b91ea2468ae974a13e6fb8f8f4dc13a
Afobaboa <120049289+Afobaboa@users.noreply.github.com>
2024-08-16 10:18:08 +0400

    Add files via upload
    main.s is redisigned

--- main.c
+++ main.c
@@ -5,28 +5,79 @@
 #include "squareSolver.h"
 
 
-bool showMenu(answer_t* answer);
+enum USER_SELECTION {
+    CONTINUE,
+    EXIT,
+    RESELECTION
+};
+typedef enum USER_SELECTION userSelection_t;
+
+
+static bool showMenu(answer_t* answer);
+static userSelection_t getUserSelection();
+static bool processUserSelection(userSelection_t userSelection);
+static void printRoots(rootCount_t rootCount, answer_t* answer);
 
 
 int main() {
     answer_t* const answer = calloc(MAX_ROOT_COUNT, sizeof(double));
-    helloMessage();
+    printHelloMessage();
     while(showMenu(answer)); // showMenu() return false if it's end
-    endMessage();
+    printEndMessage();
     free(answer);
     return 0;
 }
 
 
 // Show menu
-bool showMenu(answer_t* answer) {
+static bool showMenu(answer_t* answer) {
+    bool continueSolving = true; // The resut of this function
     puts("Для решения уравнения ax^2 + bx + c = 0, введите a, b, c по порядку:");
     double a = getDoubleValue();
     double b = getDoubleValue();
     double c = getDoubleValue();
     rootCount_t rootCount = solveEquation(a, b, c, answer);
+    printRoots(rootCount, answer);
 
-    switch (rootCount) { /* Will be added separate function */
+    userSelection_t userSelection;
+    while ((userSelection = getUserSelection()) == RESELECTION) 
+        continueSolving = processUserSelection(userSelection);
+    return continueSolving;
+}
+
+
+// Process user's selection 
+static bool processUserSelection(userSelection_t userSelection) {
+    switch (userSelection) {
+    case CONTINUE:
+        return true;
+    case EXIT:
+        return false;
+    case RESELECTION:
+        return false;
+    }
+}
+
+
+// Get user selection 
+static userSelection_t getUserSelection() {
+    printSelectionMessage();
+    int userSelection = getIntValue();
+    switch (userSelection) {
+    case 1: 
+        return CONTINUE;
+    case 2:
+        return EXIT;
+    default:
+        puts("Такого варианта нет. Попробуйте еще раз.\n");
+        return RESELECTION; 
+    }
+}
+
+
+// Print roots of user's equation
+static void printRoots(rootCount_t rootCount, answer_t* answer) {
+    switch (rootCount) {
     case INFINITE_ROOT_COUNT: 
         puts("Все действительные числа являются корнями.\n");
         break;
@@ -39,19 +90,5 @@ bool showMenu(answer_t* answer) {
     case TWO_ROOTS:
         printf("Корни данного уравнения x1 = %f, x2 = %f\n", answer[0], answer[1]);
         break;
-    }
-
-    puts("Хотите продолжить решать уравнения? Введите нужную цифру.\n" /* Will be used messages.h */
-         "1 - да, хочу продолжить.\n"
-         "2 - нет, хочу выйти.\n");
-    int userSelection = getIntValue();
-    switch (userSelection) {
-    case 1: 
-        return true;
-    case 2:
-        return false;
-    default: /* Reselection will be added in separate function */
-        puts("Такого варианта нет. Предположим, вы хотите продолжить...\n");
-        return true; 
     }
 }
--- messages.h
+++ messages.h
@@ -3,19 +3,28 @@
 
 
 
-void helloMessage();
-void endMessage();
+void printHelloMessage();
+void printEndMessage();
+void printSelectionMessage();
 
 
 // Print helloMessage when program starts
-void helloMessage() {
+void printHelloMessage() {
     puts("Эта программа умеет решать квадратные уравнения в вещественных числах! Попоробуй сам!");
 }
 
 
 // Print endMessage when program ends
-void endMessage() {
+void printEndMessage() {
     puts("Пока!");
+}
+
+
+// Print seletionMessage
+void printSelectionMessage() {
+    puts("Хотите продолжить решать уравнения? Введите нужную цифру.\n"
+         "1 - да, хочу продолжить.\n"
+         "2 - нет, хочу выйти.\n");
 }
 
 
